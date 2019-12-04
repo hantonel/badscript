46737c5db865c3321c7cd8dc08b1c1e623c60f83
hantonel <49806274+hantonel@users.noreply.github.com>
2019-12-04 10:03:49 +0000

    Add files via upload

---
+++ 20-CMT-place_bootloader.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+me=$(basename $BASH_SOURCE)
+echo "----- $me [[START]]"
+
+sudo cp /tmp/cmt_packer_bootloader.py /root
+sudo chmod +x /root/cmt_packer_bootloader.py
+sudo cp /tmp/cmt-packer-bootloader.service /lib/systemd/system/cmt-packer-bootloader.service
+
+sudo cp /tmp/systemd_env_wrapper.sh /usr/bin
+sudo chmod +x /usr/bin/systemd_env_wrapper.sh
+
+sudo systemctl daemon-reload
+sudo systemctl enable /lib/systemd/system/cmt-packer-bootloader.service
+
+echo "----- $me [[DONE]]"
